ff09ba3a91cdcc610fe3cf0f54df5012f6652aa8
Olivia <ohendrick1223@gmail.com>
2016-10-20 10:22:14 -0600

    got it working!!!!!!!!!!! initial

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
+'use strict';
+$(document).ready(function()  {
 /*
   Write JS to make this stoplight work.
 
   When I click on the 'stop' button,
+
     the top light should turn red.
+
   When I click on the 'slow' button
+
     the middle light should turn orange.
+
   When I click on the 'go' button
+
     the bottom light should turn green.
 */
+
+  var stopLight = $('#stopLight');
+  var slowLight = $('#slowLight');
+  var goLight = $('#goLight');
+
+  var turnRed = function() {
+    stopLight.css("backgroundColor", "red");
+    slowLight.css("backgroundColor", "black");
+    goLight.css("backgroundColor", "black");
+  };
+  var turnYellow = function() {
+    slowLight.css("backgroundColor", "yellow");
+    goLight.css("backgroundColor", "black");
+    stopLight.css("backgroundColor", "black");
+  };
+  var turnGreen = function() {
+    goLight.css("backgroundColor", "green");
+    slowLight.css("backgroundColor", "black");
+    stopLight.css("backgroundColor", "black");
+  };
+
+  $("#stopButton").on("click", turnRed);
+  $("#slowButton").on("click", turnYellow);
+  $("#goButton").on("click", turnGreen);
+
+
+
+
+});
